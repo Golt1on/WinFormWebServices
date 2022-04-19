05ae90949a2b4c9d34998af2535cb8a3bfcf1402
mtugra <103566384+mtugra@users.noreply.github.com>
2022-04-19 14:18:07 +0300

    Windown Form Web Services

---
+++ WinFormWebServices/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WinFormWebServices
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_tc = new System.Windows.Forms.Label();
+            this.lbl_isim = new System.Windows.Forms.Label();
+            this.lbl_soyisim = new System.Windows.Forms.Label();
+            this.lbl_yil = new System.Windows.Forms.Label();
+            this.txt_tc = new System.Windows.Forms.TextBox();
+            this.txt_isim = new System.Windows.Forms.TextBox();
+            this.txt_soyisim = new System.Windows.Forms.TextBox();
+            this.txt_yil = new System.Windows.Forms.TextBox();
+            this.btn_gonder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_tc
+            // 
+            this.lbl_tc.AutoSize = true;
+            this.lbl_tc.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_tc.Location = new System.Drawing.Point(79, 68);
+            this.lbl_tc.Name = "lbl_tc";
+            this.lbl_tc.Size = new System.Drawing.Size(31, 16);
+            this.lbl_tc.TabIndex = 0;
+            this.lbl_tc.Text = "TC:";
+            // 
+            // lbl_isim
+            // 
+            this.lbl_isim.AutoSize = true;
+            this.lbl_isim.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_isim.Location = new System.Drawing.Point(79, 127);
+            this.lbl_isim.Name = "lbl_isim";
+            this.lbl_isim.Size = new System.Drawing.Size(41, 16);
+            this.lbl_isim.TabIndex = 1;
+            this.lbl_isim.Text = "İSİM:";
+            // 
+            // lbl_soyisim
+            // 
+            this.lbl_soyisim.AutoSize = true;
+            this.lbl_soyisim.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_soyisim.Location = new System.Drawing.Point(79, 188);
+            this.lbl_soyisim.Name = "lbl_soyisim";
+            this.lbl_soyisim.Size = new System.Drawing.Size(72, 16);
+            this.lbl_soyisim.TabIndex = 2;
+            this.lbl_soyisim.Text = "SOYİSİM:";
+            // 
+            // lbl_yil
+            // 
+            this.lbl_yil.AutoSize = true;
+            this.lbl_yil.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_yil.Location = new System.Drawing.Point(79, 247);
+            this.lbl_yil.Name = "lbl_yil";
+            this.lbl_yil.Size = new System.Drawing.Size(33, 16);
+            this.lbl_yil.TabIndex = 3;
+            this.lbl_yil.Text = "YIL:";
+            // 
+            // txt_tc
+            // 
+            this.txt_tc.Location = new System.Drawing.Point(200, 64);
+            this.txt_tc.Name = "txt_tc";
+            this.txt_tc.Size = new System.Drawing.Size(288, 22);
+            this.txt_tc.TabIndex = 4;
+            // 
+            // txt_isim
+            // 
+            this.txt_isim.Location = new System.Drawing.Point(200, 126);
+            this.txt_isim.Name = "txt_isim";
+            this.txt_isim.Size = new System.Drawing.Size(288, 22);
+            this.txt_isim.TabIndex = 5;
+            // 
+            // txt_soyisim
+            // 
+            this.txt_soyisim.Location = new System.Drawing.Point(200, 187);
+            this.txt_soyisim.Name = "txt_soyisim";
+            this.txt_soyisim.Size = new System.Drawing.Size(288, 22);
+            this.txt_soyisim.TabIndex = 6;
+            // 
+            // txt_yil
+            // 
+            this.txt_yil.Location = new System.Drawing.Point(200, 246);
+            this.txt_yil.Name = "txt_yil";
+            this.txt_yil.Size = new System.Drawing.Size(288, 22);
+            this.txt_yil.TabIndex = 7;
+            // 
+            // btn_gonder
+            // 
+            this.btn_gonder.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_gonder.Location = new System.Drawing.Point(235, 318);
+            this.btn_gonder.Name = "btn_gonder";
+            this.btn_gonder.Size = new System.Drawing.Size(117, 50);
+            this.btn_gonder.TabIndex = 8;
+            this.btn_gonder.Text = "GÖNDER";
+            this.btn_gonder.UseVisualStyleBackColor = true;
+            this.btn_gonder.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(592, 414);
+            this.Controls.Add(this.btn_gonder);
+            this.Controls.Add(this.txt_yil);
+            this.Controls.Add(this.txt_soyisim);
+            this.Controls.Add(this.txt_isim);
+            this.Controls.Add(this.txt_tc);
+            this.Controls.Add(this.lbl_yil);
+            this.Controls.Add(this.lbl_soyisim);
+            this.Controls.Add(this.lbl_isim);
+            this.Controls.Add(this.lbl_tc);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "TC KİMLİK DOĞRULAMA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_tc;
+        private System.Windows.Forms.Label lbl_isim;
+        private System.Windows.Forms.Label lbl_soyisim;
+        private System.Windows.Forms.Label lbl_yil;
+        private System.Windows.Forms.TextBox txt_tc;
+        private System.Windows.Forms.TextBox txt_isim;
+        private System.Windows.Forms.TextBox txt_soyisim;
+        private System.Windows.Forms.TextBox txt_yil;
+        private System.Windows.Forms.Button btn_gonder;
+    }
+}
+
---
+++ WinFormWebServices/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormWebServices
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+             - Web service'ler http protokolünü kullanarak hizmet sağlar
+             - İstemci yani client istek atar ve bir geri dönüş gerçekleşir
+             - Bu geri dönüşte kullanılan servise göre XML, JSON vb yapılar kıllanılır
+             - Ve bilgi alışverişi sağlanmış olur.
+             - Burada textboxlar üzerinden girilen veriler alınıp sorgulama yapılır ve geri dönüş olarak 
+             true ya da false bir değer döndürülür.
+             - Ayrıca projemize url ile service refenrence ekledik
+             */
+
+            long kimlikNo = long.Parse(txt_tc.Text);
+            int yil = int.Parse(txt_yil.Text);
+            bool durum;
+
+            try
+            {
+                using (TcDogrula.KPSPublicSoapClient service = new TcDogrula.KPSPublicSoapClient { })
+                {
+                    durum = service.TCKimlikNoDogrula(kimlikNo, txt_isim.Text, txt_soyisim.Text, yil);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            MessageBox.Show(durum.ToString());
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
